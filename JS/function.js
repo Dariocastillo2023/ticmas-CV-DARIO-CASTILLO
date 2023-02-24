50ff0ec2e770a4d31d249073771018934c00957e
DARIO <darioac1@hotmail.com>
2023-02-24 04:38:07 -0300

    Se agrega el Proyecto

---
+++ JS/function.js
@@ -0,0 +1,22 @@
+  /**
+   document.getElementById("boton").onclick = function () {
+    console.log ("captura de evento")
+    document.getElementById("demo").innerHTML = "BACHILLER CONTABLE.";
+   */
+  
+  document.addEventListener("click",function(){
+    document.getElementById("demo").innerHTML = "BACHILLER CONTABLE.";
+  })
+
+  document.getElementById("boton34").addEventListener("click",function(){
+    document.body.style.backgroundColor= "YELLOW";});
+
+    
+  document.getElementById("boton221").addEventListener("click",function(){
+    document.body.style.backgroundColor= "#e9f3f5";});
+
+
+
+
+      
+   
